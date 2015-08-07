e45fdb924b57596d02652fbdf88dab3458bb4d3e
ryym <ryym64@gmail.com>
2015-08-07 16:24:01 +0900

    Add a setup script for Windows

---
+++ setup.bat
@@ -0,0 +1,20 @@
+@echo off
+
+echo This batch must be executed as administrator (to 'mklink').
+set /p ans=Are you administrator? (Y/n)
+if "%ans%" == "n" (
+    echo Cancelled.
+    set ans=
+    exit /b
+)
+
+pushd "%0\.."
+
+mklink ..\.gitconfig dotfiles\.gitconfig
+mklink /d ..\vimfiles dotfiles\.vim
+mklink ..\.ideavimrc dotfiles\.ideavimrc
+
+echo Installed.
+pause
+
+popd
