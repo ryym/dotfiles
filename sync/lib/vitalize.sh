8706e4b40f47961b6629b0ac48b5af8e1bff8c62
ryym <ryym.64@gmail.com>
2016-03-28 17:39:44 +0900

    sync: Add logger methods

--- sync/lib/vitalize.sh
+++ sync/lib/vitalize.sh
@@ -31,3 +31,43 @@ log() {
 teelog() {
     cat - | tee -a "$SYNCLOG"
 }
+
+print_logo() {
+echo '
+==============================================
+----------------------------------------------
+        _       _    __ _ _
+     __| | ___ | |_ / _(.) | ___  ___
+    / _` |/ _ \| __| |_| | |/ \ \/ __|
+   | (_| | (_) | |_|  _| | |  __/\__ \
+    \..,.|\.../ \..|.| |.|.|\...||.../
+                                       @ryym
+
+         github.com/ryym/dotfiles
+==============================================
+'
+}
+
+log_start() {
+    log_info "START: $(c_cyan "$1")" | teelog
+}
+
+log_finish() {
+    log_info "FINISH: $(c_cyan "$1")" | teelog
+}
+
+log_info() {
+    echo [$(c_grey $(date '+%T'))] "$@" | teelog
+}
+
+log_success() {
+    log_info "$(c_green "$@")" | teelog
+}
+
+log_warn() {
+    log_info "$(c_yellow "$@")" | teelog
+}
+
+log_error() {
+    log_info "$(c_red "$@")" | teelog
+}
