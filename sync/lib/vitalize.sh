23905f21d129d9ad71ff3b2b138fb768e55eb711
ryym <ryym.64@gmail.com>
2016-03-28 11:12:12 +0900

    sync: Load common utilities

---
+++ sync/lib/vitalize.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Load basic utilities.
+source "$DOTPATH/lib/vitalize.sh"
+
+# Source the specified file from $DOTPATH/sync.
+load() {
+    source "$DOTPATH/sync/$1"
+}
+
+detect_osname() {
+    if is_osx; then
+        echo 'osx'
+    elif is_debian; then
+        echo 'debian'
+    fi
+}
+
+is_osx() {
+    return $(test $(uname) == 'Darwin')
+}
+
+is_debian() {
+    return $(test -f /etc/debian_version)
+}
