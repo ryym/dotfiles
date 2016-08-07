65c490bbaf9641475dfe3489e41a7c0c542c1213
ryym <ryym.64@gmail.com>
2016-08-07 10:49:17 +0900

    sync: Add simple test script

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# A simple test script. This just check if:
+#   - all the dotfiles to be linked exist
+#   - all installation bash scripts are not broken
+# So the test success doesn't mean the installations work correctly
+# on every platforms.
+
+link/check_dotfiles
+
+for ospath in sync/os/*; do
+    OS=${ospath##*/} make dryrun-install
+    echo
+done
