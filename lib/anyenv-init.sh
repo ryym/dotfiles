8e5eb115034c1afaee7a6b76ba13a3bddc834486
ryym <ryym.64@gmail.com>
2019-04-28 23:34:44 +0900

    sync(env): Add pyenv as easy Python installer

--- lib/anyenv-init.sh
+++ lib/anyenv-init.sh
@@ -18,7 +18,7 @@ export PATH="/Users/ryu/.anyenv/envs/nodenv/bin:$PATH"
 export PATH="/Users/ryu/.anyenv/envs/nodenv/shims:${PATH}"
 export NODENV_SHELL=zsh
 source '/Users/ryu/.anyenv/envs/nodenv/libexec/../completions/nodenv.zsh'
-# command nodenv rehash 2>/dev/null
+(command nodenv rehash 2>/dev/null &)
 nodenv() {
   local command
   command="${1:-}"
@@ -33,12 +33,32 @@ nodenv() {
     command nodenv "$command" "$@";;
   esac
 }
+export PYENV_ROOT="/Users/ryu/.anyenv/envs/pyenv"
+export PATH="/Users/ryu/.anyenv/envs/pyenv/bin:$PATH"
+export PATH="/Users/ryu/.anyenv/envs/pyenv/shims:${PATH}"
+export PYENV_SHELL=zsh
+source '/Users/ryu/.anyenv/envs/pyenv/libexec/../completions/pyenv.zsh'
+(command pyenv rehash 2>/dev/null &)
+pyenv() {
+  local command
+  command="${1:-}"
+  if [ "$#" -gt 0 ]; then
+    shift
+  fi
+
+  case "$command" in
+  rehash|shell)
+    eval "$(pyenv "sh-$command" "$@")";;
+  *)
+    command pyenv "$command" "$@";;
+  esac
+}
 export RBENV_ROOT="/Users/ryu/.anyenv/envs/rbenv"
 export PATH="/Users/ryu/.anyenv/envs/rbenv/bin:$PATH"
 export PATH="/Users/ryu/.anyenv/envs/rbenv/shims:${PATH}"
 export RBENV_SHELL=zsh
 source '/Users/ryu/.anyenv/envs/rbenv/libexec/../completions/rbenv.zsh'
-# command rbenv rehash 2>/dev/null
+(command rbenv rehash 2>/dev/null &)
 rbenv() {
   local command
   command="${1:-}"
