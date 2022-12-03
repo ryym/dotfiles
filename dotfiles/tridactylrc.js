bf6577ade11ce6071e08f2a2c7a2cd04323a1d8a
ryym <ryym.64@gmail.com>
2022-12-03 15:44:07 +0900

    chore: Fix typo

--- dotfiles/tridactylrc.js
+++ dotfiles/tridactylrc.js
@@ -1,15 +1,15 @@
 // see :help js
 
 tri.browserBg.runtime.getPlatformInfo().then(os => {
-  // Adjust keybindings for Linux based on my xremap configuration.
+  // Adjust keybindings for Linux based on my Xremap configuration.
   // Currently I am a macOS user so I want to:
   //   - use Super (Command / Win) key for basic shortcuts such as copy paste.
   //   - use Ctrl key for moving cursor on input such as Ctrl-f/b that moves cursor left/right.
-  // To achieve that, I use xremap to customize key input (except on Terminal). For example:
+  // To achieve that, I use Xremap to customize key input (except on Terminal). For example:
   //   - remap Ctrl-f to Arrow-left, Ctrl-b to Arrow-right, etc.
   //   - remap Super-c to Ctrl-c, Super-f to Ctrl-f, etc.
   //   - (I cannot just remap Super to Ctrl since it breaks Super+Tab behavior)
-  // Therefore, I need to make Tridactyl handle keys remapped by xremap.
+  // Therefore, I need to make Tridactyl handle keys remapped by Xremap.
   if (os.os === 'linux') {
     tri.excmds.bind('<ArrowRight>', 'scrollpage 1') // <C-f>
     tri.excmds.bind('<ArrowLeft>', 'scrollpage -1') // <C-b>
