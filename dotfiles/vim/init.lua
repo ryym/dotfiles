5e9c20f333109d0cc35cf759b6dde6f6b1eaeb90
ryym <ryym.64@gmail.com>
2023-11-13 20:40:40 +0900

    feat(neovim): Replace init.vim with init.lua to use Lua plugins

---
+++ dotfiles/vim/init.lua
@@ -0,0 +1,45 @@
+local vim_dir = os.getenv("HOME") .. '/.vim'
+vim.cmd('source ' .. vim_dir .. '/vimrc')
+
+-- To use Neovim plugins, install by 'git clone' in 'pack/neovim/opt' manually for now.
+local packadd = function (name)
+    vim.cmd('packadd ' .. name)
+    local doc_dir = vim_dir .. '/pack/neovim/opt/' .. name .. '/doc'
+    if vim.fn.isdirectory(doc_dir) then
+        vim.cmd('helptags ' .. doc_dir)
+    end
+end
+
+-- https://github.com/sainnhe/everforest
+packadd('everforest')
+vim.cmd('colorscheme everforest')
+
+-- https://github.com/nvim-treesitter/nvim-treesitter/
+packadd('nvim-treesitter')
+require('nvim-treesitter.configs').setup({
+    ensure_installed = {
+        "go",
+        "javascript",
+        "ruby",
+        "rust",
+        "tsx",
+        "typescript",
+    },
+    highlight = {
+        enable = true,
+    },
+})
+
+-- https://github.com/lukas-reineke/indent-blankline.nvim
+packadd('indent-blankline')
+local hooks = require("ibl.hooks")
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "IndentLine1", { fg = "#4f585e" })
+    vim.api.nvim_set_hl(0, "IndentLine2", { fg = "#4e6878" })
+end)
+require('ibl').setup({
+    indent = {
+        char = '▏',
+        highlight = { "IndentLine1", "IndentLine2" },
+    },
+})
