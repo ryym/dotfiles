5254221b319cf83a815901397b4138247c0a14b1
ryym <ryym.64@gmail.com>
2025-09-13 19:00:57 +0900

    feat(fzf): Experimentally introduce Vim-like mini modal system

--- dotfiles/fzf/config.sh
+++ dotfiles/fzf/config.sh
@@ -4,13 +4,10 @@ export FZF_DEFAULT_OPTS=$(
     fzf_options=(
         '--ignore-case'
         '--cycle'
-        # Styles
         '--layout=reverse'
         '--ansi'
         '--highlight-line'
-        "--prompt='❯ '"
         '--color=current-bg:#3d484d,pointer:#e67e80,marker:#d699b6,gutter:-1'
-        # Key bindings
         '--bind=ctrl-k:kill-line'
         '--bind=ctrl-d:half-page-down'
         '--bind=ctrl-u:half-page-up'
@@ -23,7 +20,45 @@ export FZF_DEFAULT_OPTS=$(
         '--bind=alt-down:preview-bottom'
         '--bind=alt-/:toggle-wrap'
     )
-    echo "${fzf_options[@]}"
+
+    # Enable Vim-like modal behavior using unbind/rebind actions.
+    key_list=(
+        'a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm'
+        'n' 'o' 'p' 'q' 'r' 's' 't' 'u' 'v' 'w' 'x' 'y' 'z'
+        'A' 'B' 'C' 'D' 'E' 'F' 'G' 'H' 'I' 'J' 'K' 'L' 'M'
+        'N' 'O' 'P' 'Q' 'R' 'S' 'T' 'U' 'V' 'W' 'X' 'Y' 'Z'
+        'space' '<' '>' '[' ']' '{' '}' '/' '?'
+    )
+    keys=$(IFS=','; echo "${key_list[*]}")
+    custom_mode_prompt='⚡'
+    filter_mode_prompt='❯ '
+    # During the custom mode, normal keys do nothing by default.
+    for key in "${key_list[@]}"; do
+        fzf_options+=("--bind='$key:ignore'")
+    done
+    fzf_options+=(
+        "--prompt='$filter_mode_prompt'"
+        # Start with the filter mode.
+        "--bind='start:change-prompt($filter_mode_prompt)+unbind($keys)'"
+        # Switch modes by `i` and `Esc`.
+        "--bind='i:change-prompt($filter_mode_prompt)+unbind($keys)'"
+        "--bind='esc:change-prompt($custom_mode_prompt)+rebind($keys)'"
+        # Define default key bindings for the custom mode.
+        "--bind='j:down'"
+        "--bind='k:up'"
+        "--bind='h:up+up+up'"
+        "--bind='l:down+down+down'"
+        "--bind='g:first'"
+        "--bind='G:last'"
+        "--bind='J:preview-down+preview-down+preview-down'"
+        "--bind='K:preview-up+preview-up+preview-up'"
+        "--bind='H:preview-half-page-up'"
+        "--bind='L:preview-half-page-down'"
+        "--bind='<:preview-top'"
+        "--bind='>:preview-bottom'"
+    )
+
+    echo "${fzf_options[*]}"
 )
 
 
