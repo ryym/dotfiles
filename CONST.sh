a42dd801d50923f30cd492477fc148cccb28f59e
ryym <ryym.64@gmail.com>
2026-01-11 10:47:07 +0900

    feat: Merge ~/local into ~/.local
    In the past I defined the `~/local` directory as a place to put local
    tools not owned by root. But there is an XDG convention which specifies
    `~/.local/bin`, `~/.local/share`, etc for similar purposes and some
    tools follows it.
    https://specifications.freedesktop.org/basedir/latest/
    
    So I removed my own convention of `~/local` and merged it to `~/.local`.

--- CONST.sh
+++ CONST.sh
@@ -4,5 +4,5 @@ export DOTPATH=${DOTPATH:-$HOME/.dotfiles}
 
 export GOPATH=$HOME/ghq
 
-export MISE_EXE_PATH="$HOME/local/bin/mise"
+export MISE_EXE_PATH="$HOME/.local/bin/mise"
 export MISE_CONFIG_PATH="$HOME/.config/mise"
