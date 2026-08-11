b4c0408551b29ac26f75720e19f4eb4991ce9a3a
ryym <ryym.64@gmail.com>
2026-08-11 14:31:07 +0900

    feat(vim): Add command to open diff against selected branch

--- dotfiles/vim/autoload/my/plug/diffview.lua
+++ dotfiles/vim/autoload/my/plug/diffview.lua
@@ -1,3 +1,15 @@
+local function diffview_open_for_branch()
+    vim.fn['fzf#run']({
+        source = "git branch --format='%(refname:short)'",
+        sink = function(branch)
+            vim.cmd('DiffviewOpen ' .. branch)
+        end,
+        up = '45%',
+        options = '--header "[base branch for diff]"',
+    })
+end
+
+
 local function configure()
   return {
     repo = 'sindrets/diffview.nvim',
@@ -7,6 +19,7 @@ local function configure()
         })
         vim.keymap.set('n', '\\[git]gd', ':<C-u>DiffviewOpen<CR>')
         vim.keymap.set('n', '\\[git]gc', ':<C-u>DiffviewClose<CR>')
+        vim.keymap.set('n', '\\[git]gb', diffview_open_for_branch)
     end,
   }
 end
